b2fcb76de9b3ac3fedbaeea17221ebf026706bc0
wjj1660 <1661088480@qq.com>
2019-04-18 16:14:47 +0800

    第一次提交项目

---
+++ src/main/java/com/aaa/dept/Hello.java
@@ -0,0 +1,13 @@
+package com.aaa.dept;
+
+/**
+ * fileName:Hello
+ * description:
+ * author:wjj
+ * createTime:2019-04-18 下午 4:05
+ */
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
